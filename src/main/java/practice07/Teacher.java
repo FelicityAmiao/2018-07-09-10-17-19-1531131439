199238b78384adb54aa3fbb7f79908aaf30edf71
FelicityAmiao <1760077814@qq.com>
2019-07-12 00:11:38 +0800

    finish practice 7

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,35 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String classShow = this.klass == null ? "teach No Class" : "teach Class " + this.klass.getNumber();
+        return getTeacherIntroduction(classShow);
+    }
+
+    private String getTeacherIntroduction(String msg) {
+        return super.introduce() + " I am a Teacher. I " + msg + ".";
+    }
+
+    public String introduceWith(Student student) {
+        return student.getKlass().getNumber() == this.klass.getNumber() ?
+                getTeacherIntroduction("teach " + student.getName()) :
+                getTeacherIntroduction("don't teach " + student.getName());
+
+    }
 }
